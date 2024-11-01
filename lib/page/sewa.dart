055c30e421288abb14d2e81a348077f1ec779b5e
Fedrik Pangestu <fedrikpangestu1@gmail.com>
2024-11-01 11:52:56 +0700

    Version 1,1

---
+++ lib/page/sewa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sewa extends StatefulWidget {
+  const Sewa({super.key});
+
+  @override
+  State<Sewa> createState() => _SewaState();
+}
+
+class _SewaState extends State<Sewa> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
